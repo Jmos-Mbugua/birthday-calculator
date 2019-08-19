75b19ba275245d57f9f2efc5b85771bff0c3ba3d
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-08-19 07:39:33 +0300

    Modify validation form

---
+++ js/script.js
@@ -0,0 +1,8 @@
+    // form validation function
+    function validateForm() {
+        var gender = document.getElementsByName("gender");
+        var mdate = document.getElementById("day");
+        var mmonth = document.getElementById("month");
+        var myear = document.getElementById("year");
+        var formValid = false;
+        var i = 0;
