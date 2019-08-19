5f85a23984f6cb751eed05df4cdedd9878b53835
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-08-19 07:42:14 +0300

    Validate date of birth

--- script.js
+++
@@ -1,9 +0,0 @@
-function validateForm () {
-    var year = document.getElementById("inputyear4");
-    var month = document.getElementById("inputmonth4");
-    var day = document.getElementById("inputday4");
-
-    if(year.value == "" || month.value == "" || day.value == ""){
-        alert("Please fill in your details");
-    }
-}
